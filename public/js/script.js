30b2ecd5e9e5b1c641f7e1500981e7a077cc9ee7
LenitoArruda <lenitoarruda@hotmail.com>
2022-11-30 21:43:46 -0300

    Design

--- public/js/script.js
+++ public/js/script.js
@@ -11,20 +11,6 @@ function animeScroll(){
     });
 }
 
-function formatDate(data){
-	var dia = data.getDate();
-	if (dia.toString().length == 1){
-		dia = "0"+dia;
-	}
-	var mes = data.getMonth()+1;
-	if (mes.toString().length == 1){
-		mes = "0"+mes;
-	}
-	var ano = data.getFullYear();  
-	
-	return dia+"/"+mes+"/"+ano;
-}
-
 animeScroll();
 window.addEventListener('scroll', function() {
     animeScroll();
